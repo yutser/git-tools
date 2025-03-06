699f19205f7dea1ff11c15346c2b6e4ecc5358f3
Sagara Yuta <Yuta.Sagara@sony.com>
2025-03-06 23:59:57 +0900

    複数文字列の置換および削除機能をbulk-repo-update.shに追加

--- bulk-update/bulk-repo-update.sh
+++ bulk-update/bulk-repo-update.sh
@@ -17,10 +17,9 @@ fi
 
 # YAMLファイルから設定を読み込む
 TARGET_FILE=$(yq e '.target-file' "$REPO_LIST")
-STRING_A=$(yq e '.string-a' "$REPO_LIST")
-STRING_B=$(yq e '.string-b' "$REPO_LIST")
 NEW_BRANCH=$(yq e '.new-branch' "$REPO_LIST")
 repos=$(yq e '.repositories[].name' "$REPO_LIST")
+strings=$(yq e '.strings' "$REPO_LIST")
 
 # 現在のディレクトリ以下を検索
 for REPO_NAME in $repos; do
@@ -29,15 +28,24 @@ for REPO_NAME in $repos; do
     cd "$REPO_PATH" || exit
     # 新しいブランチを作成
     git checkout -b "$NEW_BRANCH"
-    # 指定されたファイルの文字列を置換
-    sed -i '' "s/$STRING_A/$STRING_B/g" "$TARGET_FILE"
+    # 指定されたファイルの文字列を置換または削除
+    for row in $(echo "${strings}" | yq -o=json | jq -c '.[]'); do
+      OLD_STRING=$(echo "${row}" | jq -r '.old')
+      NEW_STRING=$(echo "${row}" | jq -r '.new // empty')
+      ACTION=$(echo "${row}" | jq -r '.action')
+      if [ "$ACTION" == "replace" ]; then
+        sed -i '' "s/$OLD_STRING/$NEW_STRING/g" "$TARGET_FILE"
+      elif [ "$ACTION" == "delete" ]; then
+        sed -i '' "/$OLD_STRING/d" "$TARGET_FILE"
+      fi
+    done
     # 変更をコミット
     git add "$TARGET_FILE"
-    git commit -m "Replace $STRING_A with $STRING_B in $TARGET_FILE"
+    git commit -m "Update strings in $TARGET_FILE"
     # リモートにプッシュ
     git push origin "$NEW_BRANCH"
     # プルリクエストを作成
-    gh pr create --base main --head "$NEW_BRANCH" --title "Replace $STRING_A with $STRING_B" --body "This PR replaces all occurrences of $STRING_A with $STRING_B in $TARGET_FILE."
+    gh pr create --base main --head "$NEW_BRANCH" --title "Update strings in $TARGET_FILE" --body "This PR updates multiple strings in $TARGET_FILE."
     # 元のディレクトリに戻る
     cd - || exit
   else
